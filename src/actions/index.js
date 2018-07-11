7df34d18ac52be43c076313f65f3a9b41832bfc9
Tráng Hà Viết <viettrangha@gmail.com>
2018-07-11 14:06:35 +0700

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,45 @@
+// Actions are payloads of information that send data from your application to your store.
+// They are the only source of information for the store.
+/*
+ * action types
+ */
+
+export const ADD_TODO = 'ADD_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+let nextToDoId = 0
+
+/*
+* Action creators: create actions. factory pattern.
+*/
+export const addToDo = text => ({
+  type: ADD_TODO, // action type
+  id: nextToDoId++,
+  text,
+})
+
+export const removeToDo = id => ({
+  type: REMOVE_TODO,
+  id,
+})
+
+export const setVisibilityFilter = filter => ({
+  type: SET_VISIBILITY_FILTER,
+  filter,
+})
+
+export const toggleToDo = id => ({
+  type: TOGGLE_TODO,
+  id,
+})
+
+/*
+* other constants
+*/
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE',
+}
