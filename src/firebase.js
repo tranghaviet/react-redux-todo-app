c2a79109b2cc99fa45882332375b694f637bfc11
Tráng Hà Viết <viettrangha@gmail.com>
2018-07-12 17:27:50 +0700

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+  // TODO: replace by your configuration
+  apiKey: "crPEjFL8B8Bt2Shg_RWJgjk-Cf-IyY",
+  authDomain: "tranghv-react-redux-todo.firebaseapp.com",
+  databaseURL: "https://tranghv-react-redux-todo.firebaseio.com",
+  projectId: "tranghv-react-redux-todo",
+  storageBucket: "",
+  messagingSenderId: "441451901990"
+}
+
+firebase.initializeApp(config);
+
+export default firebase
--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
+import './firebase'
 import store from './store'
 // import {
 //   addToDo,
