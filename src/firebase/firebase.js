e8c0c66e8b8417432c121e8b03a6a316eede28aa
Tráng Hà Viết <viettrangha@gmail.com>
2018-07-13 17:14:54 +0700

    allow load config in .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  // TODO: replace by your configuration
-  apiKey: "AIzaSyCK4crPEjFL8B8Bt2Shg_RWJgjk-Cf-IyY",
-  authDomain: "tranghv-react-redux-todo.firebaseapp.com",
-  databaseURL: "https://tranghv-react-redux-todo.firebaseio.com",
-  projectId: "tranghv-react-redux-todo",
-  storageBucket: "",
-  messagingSenderId: "441451901990"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
 
 firebase.initializeApp(config);
